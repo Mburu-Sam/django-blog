344b96bcca48fd52387c4b793f3a1a955faae8ea
Mburu Sam <mburusam885@gmail.com>
2026-06-18 17:47:00 +0300

    Adding forms for CRUD oprations

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,17 @@
+# from django.urls import path
+# from .views import post_list, post_detail
+
+# urlpatterns = [
+# path("post/<int:pk>/", post_detail, name="post_detail"),
+# path("", post_list, name="home"),
+# ]
+
 from django.urls import path
-from .views import post_list, post_detail
-# new
+from . import views
 urlpatterns = [
-path("post/<int:pk>/", post_detail, name="post_detail"),
-path("", post_list, name="home"),
+    path("post/new/", views.BlogCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/", views.BlogDetailView.as_view(), name="post_detail"),
+    path("post/<int:pk>/edit/", views.BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", views.BlogDeleteView.as_view(), name="post_delete"),
+    path("", views.BlogListView.as_view(), name="home"), 
 ]
